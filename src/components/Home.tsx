906d2b3ee034349207db539e7f3ec69da16e9047
Roshan Avatirak <roshanavatirak@gmail.com>
2025-02-16 00:22:18 +0530

    Past event updated

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -142,7 +142,7 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    const eventDate = new Date("2025-02-17T23:59:59"); // Set your actual event date
+    const eventDate = new Date("2025-02-17"); // Set your actual event date
     const today = new Date();
 
     const timeDiff = eventDate.getTime() - today.getTime();
