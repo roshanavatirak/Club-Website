23893f3b76b4bc067993f435cfd867d7530bbed2
Roshan Avatirak <roshanavatirak@gmail.com>
2025-02-15 19:54:44 +0530

    core member updated

--- src/components/FutureEvents.tsx
+++ src/components/FutureEvents.tsx
@@ -84,7 +84,7 @@ function FutureEvents() {
         {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> */}
         <div className="flex justify-center">
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-5xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-5x2">
       
 
 
